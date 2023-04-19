69d07b669de9e355fb3c9e7846f14df1ac3bc456
Fernando Borin <fernandopborin@gmail.com>
2023-04-19 16:07:59 -0300

    C++

--- Exercicios Target/Ex 2.cpp
+++ Exercicios Target/Ex 2.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int n, termoAtual, termoAnterior = 1, termoPenultimo = 0;
+void fibonacci(int n, int termoAtual, int termoAnterior, int termoPenultimo){
+    cout << "Exibindo os primeiros " << n << " termos da sequencia de Fibonacci: " << termoAtual;
 
-void fibonacci(int x){
-    cout << "Exibindo os primeiros " << x << " termos da sequencia de Fibonacci: " 
-         << termoAtual;
-
-    for (int i = 0; i <= x - 2; i++){
+    for (int i = 0; i <= n - 2; i++){
         termoAtual = termoAnterior + termoPenultimo;
         termoPenultimo = termoAnterior;
         termoAnterior = termoAtual;
@@ -16,9 +13,11 @@ void fibonacci(int x){
 }
 
 int main(){
+    int n, termoAtual = 1, termoAnterior = 1, termoPenultimo = 0;
+
     cout << "Digite o numero de termos: ";
     cin >> n;
 
-    fibonacci(n);
+    fibonacci(n, termoAtual, termoAnterior, termoPenultimo);
 
 }
