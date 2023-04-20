b89b7d5b9b9e6b6b69a7dded8388d5cf01792030
Fernando Borin <fernandopborin@gmail.com>
2023-04-20 13:12:15 -0300

    Target

--- Exercicios Target/Exercicio 5.cpp
+++ Exercicios Target/Exercicio 5.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 void inverterString(string string1, string string2){
     for (int i = string1.length(); i >= 0; i--){
-        string2 = string2 + string1[i];
+        string2 = string2 + string1[i]; // Soma a string2 vazia com a string1 em um contador decrescente
     }
 
     cout << "String invertida: " << string2;
 }
 
 int main(){
-    string string1 = "", string2 = "";
+    string string1, string2;
 
     cout << "Digite a string: ";
     getline(cin, string1);
